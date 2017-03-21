e29fa0002f9e715041ff438c283a799ee7d4bbd3
diep3462000 <tran.hoang.diep@framgia.com>
2017-03-21 14:33:14 +0700

    topuser

--- app/Http/Controllers/Admin/Basic/TopUserController.php
+++ app/Http/Controllers/Admin/Basic/TopUserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin\Basic;
 use App\PurchaseMoneyLog;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 
 class TopUserController extends Controller
 {
@@ -23,14 +24,15 @@ class TopUserController extends Controller
         if($userName != ''){
             $query->where('userName','LIKE','%'.$userName.'%');
         }
-
+        $query->select("userId", "userName",DB::raw('SUM(cashValue) as sum_cash'));
         if($fromDate != '' && $toDate != ''){
             $start = date("Y-m-d",strtotime($fromDate));
             $end = date("Y-m-d",strtotime($toDate));
-            $query->whereBetween('purchasedTime',[$start,$end]);
+            $query->whereBetween('purchasedTime',[$start . " 00:00:00",$end . " 23:59:59"]);
         }
-        $data = $query->orderBy('cashValue')->paginate(10);
-
+        $query->groupBy("userId", "userName");
+        $query->orderBy("sum_cash", "desc");
+        $data = $query->paginate(10);
         return view('admin.basic.topUser.index',compact('data'))->with('i', ($request->input('page', 1) - 1) * 10);
     }
 }
